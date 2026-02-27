526352639aa2463495c651fe35cc44a48854660e
test-DD <88162409+test-DD@users.noreply.github.com>
2026-02-27 06:11:10 +0000

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -8,16 +8,26 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 
 sudo mkdir -p /etc/apt/keyrings
 
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+#sudo rm -rf /var/lib/apt/lists/*
+#sudo apt-get update
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
 
 sudo apt update -y
 
-apt-cache policy docker-ce -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo apt install docker-ce -y
+#apt-cache policy docker-ce -y
 
+#sudo apt install docker-ce -y
+#sudo apt-get update
+#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# 
 #sudo systemctl status docker
 
 sudo chmod 777 /var/run/docker.sock
