1646d8f0820cd6d86d3f5794c46ed4a4a19e7468
test-DD <88162409+test-DD@users.noreply.github.com>
2026-02-27 05:59:35 +0000

    Update docker.sh
    Added two line t update keyrings

--- docker.sh
+++ docker.sh
@@ -4,7 +4,11 @@ sudo apt update -y
 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
 
